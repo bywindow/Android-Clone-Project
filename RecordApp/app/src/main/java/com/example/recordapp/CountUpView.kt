f3a29847f3a6b8dad36a1a173e762cce0ada7098
dionidip <paulus0617@gmail.com>
2023-01-30 01:41:49 +0900

    Add: make timer to coustom view

---
+++ RecordApp/app/src/main/java/com/example/recordapp/CountUpView.kt
@@ -0,0 +1,48 @@
+package com.example.recordapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.SystemClock
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+
+class CountUpView(
+    context: Context,
+    attrs: AttributeSet
+) : AppCompatTextView(context, attrs) {
+
+    private var startTimeStamp: Long = 0L
+
+    private val countUpAction: Runnable = object: Runnable {
+        override fun run() {
+            val currentTimeStamp = SystemClock.elapsedRealtime()
+            val countTimeSeconds = ((currentTimeStamp - startTimeStamp) / 1000L).toInt()
+            updateCountTime(countTimeSeconds)
+
+            handler?.postDelayed(this, ACTION_INTERVAL)
+        }
+
+    }
+
+    fun startCountUp() {
+        startTimeStamp = SystemClock.elapsedRealtime()
+        handler?.post(countUpAction)
+    }
+
+    fun stopCountUp() {
+        handler?.removeCallbacks(countUpAction)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateCountTime(countTimeSeconds: Int) {
+        val minutes = countTimeSeconds / 60
+        val seconds = countTimeSeconds % 60
+
+        text = "02d:%02d".format(minutes, seconds)
+    }
+
+    companion object{
+        private const val ACTION_INTERVAL = 1000L
+    }
+}
